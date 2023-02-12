04b22a1f9921abc0b5a65edb51f5cf5ac1450c50
Gunnar <gunnar.slaa@gmail.com>
2023-02-12 13:13:21 +0100

    1b done

--- 2021/1/1b.rs
+++ 2021/1/1b.rs
@@ -3,12 +3,13 @@ use std::fs;
 fn main() {
     let data = fs::read_to_string("input").expect("Can't read file");
     let lines: Vec<i32> = data.split("\r\n").map(|x| x.parse::<i32>().unwrap_or(0)).collect();
-    let mut previous = &lines[0];
+    let mut previous = &lines[0] + &lines[1] + &lines[2];
     let mut count = 0;
-    for (c, num) in lines.iter().enumerate(){
-        if c == 0 {continue;}
-        if num > previous {count+=1;}
-        previous = num;
+    for c in 1..lines.len() {
+        if c < 2 {continue;}
+        let sum = &lines[c - 2] + &lines[c - 1] + &lines[c];
+        if sum > previous {count+=1;}
+        previous = sum;
     }
     println!("Count: {}", count)
 }
