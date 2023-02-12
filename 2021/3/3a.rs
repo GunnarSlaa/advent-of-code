de6bac5ca2e2a3fe99dc859fb538906f81bd4489
Gunnar <gunnar.slaa@gmail.com>
2023-02-12 19:24:33 +0100

    3a done

---
+++ 2021/3/3a.rs
@@ -0,0 +1,26 @@
+use std::fs;
+use std::convert::TryInto;
+
+fn main() {
+    let data = fs::read_to_string("input").expect("Can't read file");
+    let lines = data.split("\r\n");
+    let lines_vec: Vec<&str> = lines.clone().collect();
+    let lines_count = lines_vec.len();
+    let line_size = lines_vec[0].len();
+    let mut count_ones = vec![0; line_size];
+
+    for line in lines{
+        for (c, n) in line.chars().enumerate(){
+            if n == '1' {count_ones[c] += 1}
+        }
+    }
+    let gamma_str = count_ones.iter()
+        .map(|x| (if x > &(lines_count / 2) {1} else {0}).to_string())
+        .collect::<Vec<String>>()
+        .join("");
+    let gamma = usize::from_str_radix(&gamma_str, 2).unwrap();
+    let epsilon = 2_usize.pow(line_size.try_into().unwrap()) - 1 - gamma;
+    println!("Gamma: {}", gamma);
+    println!("Epsilon: {}", epsilon);
+    println!("Answer: {}", epsilon * gamma);
+}
