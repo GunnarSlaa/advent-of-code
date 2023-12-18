0433f485b507284f8bd21f55aff759d07e9d9588
Gunnar <gunnar.slaa@gmail.com>
2023-12-18 18:37:25 +0100

    2023 day 18

---
+++ 2023/18/18b.py
@@ -0,0 +1,30 @@
+from grid_utils import *
+
+with open("input", "r") as file:
+    lines = file.read().strip().split("\n")
+
+g = grid_from_lines(lines)
+deltas = ((0, 1), (1, 0), (0, -1), (-1, 0))
+
+loc = (0, 0)
+nodes = [loc]
+border = 0
+for line in lines:
+    dir = deltas[int(line.split()[2][-2])]
+    amount = int(line.split()[2][-7:-2], 16)
+    x = loc[0] + amount * dir[0]
+    y = loc[1] + amount * dir[1]
+    loc = (x, y)
+    nodes.append(loc)
+    border += amount
+
+nodes.append((0,0))
+nodes = nodes[::-1]
+shoelace = 0
+for i in range(len(nodes) - 1):
+    shoelace += nodes[i][0] * nodes[i + 1][1]
+    shoelace -= nodes[i][1] * nodes[i + 1][0]
+
+print(shoelace)
+print(shoelace / 2 + border / 2 + 1)
+print(border)
