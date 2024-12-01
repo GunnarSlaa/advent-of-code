898360c38be5a142da60ad4c913ccedc1e528ef1
Gunnar Slaa <gunnar.slaa@gmail.com>
2024-12-01 16:45:37 +0100

    refactored day 1

--- 2024/01/1.py
+++ 2024/01/1.py
@@ -1,16 +1,9 @@
 with open("input", "r") as file:
     lines = file.read().strip().split("\n")
 
-list1 = []
-list2 = []
-for line in lines:
-    list1.append(int(line.split()[0]))
-    list2.append(int(line.split()[1]))
+list1 = sorted([int(line.split()[0]) for line in lines])
+list2 = sorted([int(line.split()[1]) for line in lines])
 
-list1.sort()
-list2.sort()
-result = 0
-for i in range(len(list1)):
-    result += abs(list1[i] - list2[i])
+result = sum([abs(list1[i] - list2[i]) for i in range(len(list1))])
 
 print(result)
--- 2024/01/1b.py
+++ 2024/01/1b.py
@@ -1,14 +1,9 @@
 with open("input", "r") as file:
     lines = file.read().strip().split("\n")
 
-list1 = []
-list2 = []
-for line in lines:
-    list1.append(int(line.split()[0]))
-    list2.append(int(line.split()[1]))
+list1 = sorted([int(line.split()[0]) for line in lines])
+list2 = sorted([int(line.split()[1]) for line in lines])
 
-result = 0
-for i in list1:
-    result += i * list2.count(i)
+result = sum([i * list2.count(i) for i in list1])
 
 print(result)
