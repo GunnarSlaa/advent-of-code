bfe88ae50f39ff79ec1f9e5db891d88ade08de71
Gunnar Slaa <gunnar.slaa@gmail.com>
2024-12-07 21:38:20 +0100

    2024 day 7

---
+++ 2024/07/7.py
@@ -0,0 +1,17 @@
+from grid_utils import *
+
+with open("input", "r") as file:
+    lines = file.read().strip().split("\n")
+
+result = 0
+
+for line in lines:
+    test_value = int(line.split(":")[0])
+    values = [int(_) for _ in line.split(":")[1].split()]
+    outcomes = [values[0]]
+    for value in values[1:]:
+        outcomes = [outcome + value for outcome in outcomes] + [outcome * value for outcome in outcomes]
+    if test_value in outcomes:
+        result += test_value
+
+print(result)
